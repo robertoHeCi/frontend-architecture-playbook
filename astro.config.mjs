1bd5ec62bf3aa2ce57f81ea5c97312663bae71b0
roberto <roberto.hernandez@one-beyond.com>
2025-05-09 12:27:36 +0200

    fix: deployment issue related to sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Frontend Architecture Playbook',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
@@ -23,4 +23,7 @@ export default defineConfig({
 			],
 		}),
 	],
+	image: {
+		service: passthroughImageService(),
+	},
 });
