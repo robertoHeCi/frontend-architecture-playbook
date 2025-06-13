d05b0bc14f888a973d1616c64f69755756568feb
roberto <roberto.hernandez@one-beyond.com>
2025-06-13 12:01:28 +0200

    feat: update menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,73 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig, passthroughImageService } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Frontend Architecture Playbook',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-	image: {
-		service: passthroughImageService(),
-	},
+  integrations: [
+    starlight({
+      title: "Frontend Architecture Playbook",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/robertoHeCi/frontend-architecture-playbook"
+        }
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            {
+              label: "Getting Started",
+              items: [
+                { label: "Introduction", link: "/guides/getting-started/" },
+                {
+                  label: "Basic Concepts",
+                  link: "/guides/getting-started/basics"
+                },
+                {
+                  label: "Design Patterns",
+                  link: "/guides/getting-started/patterns"
+                }
+                // { label: 'State Management', link: '/guides/getting-started/state-management' },
+              ]
+            }
+            // {
+            // 	label: 'Architecture',
+            // 	items: [
+            // 		{ label: 'Layered Architecture', link: '/guides/architecture/layered' },
+            // 		{ label: 'Clean Architecture', link: '/guides/architecture/clean' },
+            // 		{ label: 'Micro Frontends', link: '/guides/architecture/micro-frontends' },
+            // 	],
+            // },
+            // {
+            // 	label: 'Examples',
+            // 	items: [
+            // 		{ label: 'Mini Commerce', link: '/guides/examples/mini-commerce' },
+            // 	],
+            // },
+          ]
+        }
+        // {
+        // label: "Reference",
+        // items: [
+        // 		{ label: 'Patterns', link: '/reference/patterns' },
+        // 		{ label: 'Best Practices', link: '/reference/best-practices' },
+        // { label: "Tools", link: "/reference/tools" }
+        // ]
+        // }
+        // {
+        // 	label: 'Resources',
+        // 	items: [
+        // 		{ label: 'Books', link: '/resources/books' },
+        // 		{ label: 'Articles', link: '/resources/articles' },
+        // 		{ label: 'Videos', link: '/resources/videos' },
+        // 	],
+        // },
+      ]
+    })
+  ],
+  image: {
+    service: passthroughImageService()
+  }
 });
